2515134940c556469dda4e4ff68d2926b38cea94
Jun Liu <jlive@Mac-for-Juneau.local>
2018-01-03 15:21:56 +0800

    test

--- tools/cert_gen.sh
+++ tools/cert_gen.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-if [ -z "${MASTER_CLUSTER_IP}" ];then
-	echo "Usage: export MASTER_CLUSTER_IP=192.168.130.100"
+if [ -z "${KUBERNETES_APISERVER}" ];then
+	echo "Usage: export KUBERNETES_APISERVER=https://192.168.130.11:6443"
 	echo "bash $0"
 	exit 1
 fi
 
-CERT_DIR=roles/kubernetes-kubeconfig/files/pki
+CN=$(echo $KUBERNETES_APISERVER|grep -o '\d\+.\d\+.\d\+.\d\+')
+CERT_DIR=files/pki
 mkdir -p $CERT_DIR
 
 echo $(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64 | tr -d "=+/" | dd bs=32 count=1 2>/dev/null),kube-admin,1 > $CERT_DIR/known_tokens.csv
@@ -13,7 +14,7 @@ echo PASS,kube-admin,1 > $CERT_DIR/basic_auth.csv
 
 # i.ca
 openssl genrsa -out $CERT_DIR/ca.key 2048
-openssl req -x509 -new -nodes -key $CERT_DIR/ca.key -subj "/CN=${MASTER_CLUSTER_IP}" -days 10000 -out $CERT_DIR/ca.crt
+openssl req -x509 -new -nodes -key $CERT_DIR/ca.key -subj "/CN=$CN" -days 10000 -out $CERT_DIR/ca.crt
 
 # ii.server
 openssl genrsa -out $CERT_DIR/server.key 2048
@@ -31,7 +32,7 @@ ST = Shanghai
 L = Shanghai
 O = Wanda., Ltd
 OU = wandayun
-CN = ${MASTER_CLUSTER_IP}
+CN = $CN
     
 [ req_ext ]
 subjectAltName = @alt_names
@@ -42,7 +43,7 @@ DNS.2 = kubernetes.default
 DNS.3 = kubernetes.default.svc
 DNS.4 = kubernetes.default.svc.cluster
 DNS.5 = kubernetes.default.svc.cluster.local
-IP.1 = ${MASTER_CLUSTER_IP}
+IP.1 = $CN
 IP.2 = 10.254.0.1
 IP.3 = 10.96.0.10
 
@@ -57,3 +58,30 @@ EOF
 openssl req -new -key $CERT_DIR/server.key -out $CERT_DIR/server.csr -config $CERT_DIR/csr.conf
 openssl x509 -req -in $CERT_DIR/server.csr -CA $CERT_DIR/ca.crt -CAkey $CERT_DIR/ca.key -CAcreateserial -out $CERT_DIR/server.crt -days 10000 -extensions v3_ext -extfile $CERT_DIR/csr.conf
 openssl x509 -noout -text -in $CERT_DIR/server.crt
+
+# -----------------------
+export KUBECONFIG_PATH=files/kubeconfig
+export CLUSTER_NICK=kubernetes
+export USER_NICK=kubelet
+export CONTEXT=$USER_NICK@$CLUSTER_NICK
+
+# Cluster
+kubectl config set-cluster $CLUSTER_NICK \
+    --server=$KUBERNETES_APISERVER \
+    --certificate-authority=$CERT_DIR/ca.crt \
+    --embed-certs=true \
+    --kubeconfig=$KUBECONFIG_PATH
+# User
+kubectl config set-credentials $USER_NICK \
+    --client-certificate=$CERT_DIR/server.crt \
+    --client-key=$CERT_DIR/server.key \
+    --embed-certs=true \
+    --kubeconfig=$KUBECONFIG_PATH
+# Context
+kubectl config set-context $CONTEXT \
+    --cluster=$CLUSTER_NICK \
+    --user=$USER_NICK \
+    --kubeconfig=$KUBECONFIG_PATH
+# Switched to context
+kubectl config use-context $CONTEXT \
+    --kubeconfig=$KUBECONFIG_PATH
